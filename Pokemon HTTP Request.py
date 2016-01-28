b0712696ee7764b9aad60f782b49354d973d21e8
Ferretsroq <Ferretsroq@gmail.com>
2016-01-28 15:48:17 -0500

    Pokemon is Now a Class
    We won't talk about how we got here, but Crystal is the best!

--- Pokemon HTTP Request.py
+++ Pokemon HTTP Request.py
@@ -4,10 +4,60 @@ import json
 from fractions import Fraction
 
 def PrintData(section, name):
-        for block in section:
-                print block['ranking'], block[name], block['usageRate']
-                listOfPercentages.append(block['usageRate']/100)
-        print "\n ---------- \n"
+    for block in section:
+        print block['ranking'], block[name], block['usageRate']
+        #listOfPercentages.append(block['usageRate']/100)
+    print "\n ---------- \n"
+
+def GetData(pokemonID):
+    requestDataList = [
+        'languageId=2',
+        'seasonId=114',
+        'battleType=5',
+        'timezone=EST',
+        'pokemonId=%d-0' % (pokemonID),
+        'displayNumberWaza=20',
+        'displayNumberTokusei=3',
+        'displayNumberSeikaku=20',
+        'displayNumberItem=20',
+        'displayNumberLevel=20',
+        'displayNumberPokemonIn=20',
+        'displayNumberPokemonDown=20',
+        'displayNumberPokemonDownWaza=20',
+        'timeStamp=1453422223192'
+        ]
+    requestDataString = "&".join(requestDataList)
+    pokemonData = json.loads(requests.post(url, data=requestDataString, headers=headersDictionary).text)
+    return pokemonData
+
+class Pokemon:
+    def __init__(self, pokemonData):
+        self.thisPokemonName = pokemonData['rankingPokemonInfo']['name']
+        self.thisPokemonRanking = pokemonData['rankingPokemonInfo']['ranking']
+        self.movesThatThisPokemonKOsWith = pokemonData['rankingPokemonSuffererWaza']
+        self.pokemonThatThisPokemonKOs = pokemonData['rankingPokemonSufferer']
+        self.pokemonOnTheSameTeamWithThisPokemon = pokemonData['rankingPokemonIn']
+        self.movesThatThisPokemonUses = pokemonData['rankingPokemonTrend']['wazaInfo']
+        self.itemsThatThisPokemonUses = pokemonData['rankingPokemonTrend']['itemInfo']
+        self.abilitiesThatThisPokemonUses = pokemonData['rankingPokemonTrend']['tokuseiInfo']
+        self.naturesThatThisPokemonUses = pokemonData['rankingPokemonTrend']['seikakuInfo']
+        self.pokemonThatKOThisPokemon = pokemonData['rankingPokemonDown']
+        self.movesThatKOThisPokemon = pokemonData['rankingPokemonDownWaza']
+        self.listOfPercentages = []
+        self.listOfDenominators = []
+        self.PopulatePercentages(self.movesThatThisPokemonKOsWith)
+        self.PopulatePercentages(self.movesThatThisPokemonUses)
+        self.PopulatePercentages(self.itemsThatThisPokemonUses)
+        self.PopulatePercentages(self.abilitiesThatThisPokemonUses)
+        self.PopulatePercentages(self.naturesThatThisPokemonUses)
+        self.PopulatePercentages(self.movesThatKOThisPokemon)
+    def __repr__(self):
+        return self.thisPokemonName
+    def PopulatePercentages(self, numericalData):
+        for element in numericalData:
+            self.listOfPercentages.append(element['usageRate']/100)
+        for percentage in self.listOfPercentages:
+            self.listOfDenominators.append((Fraction(percentage).limit_denominator()).denominator)
 
 headersDictionary = {
 	'Accept' : '*/*',
@@ -16,14 +66,14 @@ headersDictionary = {
 	'Connection' : 'keep-alive',
 	'Content-Length' : '288',
 	'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie' : '__ulfpc=201507141648558514; __utma=234147713.44858212.1436906933.1450754425.1450754425.1; __utmz=234147713.1450754425.1.1.utmcsr=pokemon-gl.com|utmccn=(referral)|utmcmd=referral|utmcct=/; region=1; language_id=2; site=2; JSESSIONID=DD14469E9FCCB8E77EC19C37921BE293; AWSELB=99C3FF770EA3504C46F25D799674203D12E259AC7AF523A441BC83B81A7A34CC74546EE73334E1A5AE0296D9AE6D620857C9DF385445A267DB7E496BEA70327F1D05B86B1023FF697977A00E295CBB8437E703A8CE; _ga=GA1.2.44858212.1436906933; _gat=1; NO_MEMBER_DATA=%7B%22language_id%22%3A2%2C%22site%22%3A2%2C%22region%22%3A1%7D',
-        'Host' : '3ds.pokemon-gl.com',
+		'Cookie' : '__ulfpc=201507141648558514; __utma=234147713.44858212.1436906933.1450754425.1450754425.1; __utmz=234147713.1450754425.1.1.utmcsr=pokemon-gl.com|utmccn=(referral)|utmcmd=referral|utmcct=/; region=1; language_id=2; site=2; JSESSIONID=DD14469E9FCCB8E77EC19C37921BE293; AWSELB=99C3FF770EA3504C46F25D799674203D12E259AC7AF523A441BC83B81A7A34CC74546EE73334E1A5AE0296D9AE6D620857C9DF385445A267DB7E496BEA70327F1D05B86B1023FF697977A00E295CBB8437E703A8CE; _ga=GA1.2.44858212.1436906933; _gat=1; NO_MEMBER_DATA=%7B%22language_id%22%3A2%2C%22site%22%3A2%2C%22region%22%3A1%7D',
+		'Host' : '3ds.pokemon-gl.com',
 	'Origin' : 'http://3ds.pokemon-gl.com',
 	'Referer' : 'http://3ds.pokemon-gl.com/battle/oras/',
 	'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
 	}
 
-requestDataList = [
+'''requestDataList = [
 	'languageId=2',
 	'seasonId=114',
 	'battleType=5',
@@ -38,10 +88,11 @@ requestDataList = [
 	'displayNumberPokemonDown=20',
 	'displayNumberPokemonDownWaza=20',
 	'timeStamp=1453422223192'
-	]
+	]'''
 
 url = "http://3ds.pokemon-gl.com/frontendApi/gbu/getSeasonPokemonDetail"
-requestDataString = "&".join(requestDataList)
+
+'''requestDataString = "&".join(requestDataList)
 r = requests.post(url, data=requestDataString, headers=headersDictionary)
 #print r.text
 
@@ -58,34 +109,14 @@ itemsThatThisPokemonUses = pokemonData['rankingPokemonTrend']['itemInfo']
 abilitiesThatThisPokemonUses = pokemonData['rankingPokemonTrend']['tokuseiInfo']
 naturesThatThisPokemonUses = pokemonData['rankingPokemonTrend']['seikakuInfo']
 pokemonThatKOThisPokemon = pokemonData['rankingPokemonDown']
-movesThatKOThisPokemon = pokemonData['rankingPokemonDownWaza']
+movesThatKOThisPokemon = pokemonData['rankingPokemonDownWaza']'''
 
-listOfPercentages = []
-listOfDenominators = []
 
-print "Pokemon Name:", thisPokemonName
-print "Rank:", thisPokemonRanking
+pokemonData = GetData(383)
+pokemonList = []
+pokemonList.append(0)
+pokemonList.append(Pokemon(pokemonData))
+
+print pokemonList[1].thisPokemonRanking, ":", pokemonList[1].thisPokemonName
 print "\n----------\n"
-
-print "These are the moves that this pokemon KOs with!"
-PrintData(movesThatThisPokemonKOsWith, 'wazaName')
-#print "These are the pokemon that this pokemon KOs!"
-#PrintData(pokemonThatThisPokemonKOs, 'name')
-#print "These are the pokemon that are on this pokemon's team!"
-#PrintData(pokemonOnTheSameTeamWithThisPokemon, 'name')
-print "These are the moves that this pokemon uses!"
-PrintData(movesThatThisPokemonUses, 'name')
-print "These are the items that this pokemon uses!"
-PrintData(itemsThatThisPokemonUses, 'name')
-print "These are the abilities that this pokemon uses!"
-PrintData(abilitiesThatThisPokemonUses, 'name')
-print "These are the natures that this pokemon uses!"
-PrintData(naturesThatThisPokemonUses, 'name')
-#print "These are the pokemon that KO this pokemon!"
-#PrintData(pokemonThatKOThisPokemon, 'name')
-#print "These are the moves that KO this pokemon!"
-#PrintData(movesThatKOThisPokemon, 'wazaName')
-
-for x in listOfPercentages:
-        listOfDenominators.append((Fraction(x).limit_denominator()).denominator)
-
+PrintData(pokemonList[1].movesThatThisPokemonUses, 'name')
