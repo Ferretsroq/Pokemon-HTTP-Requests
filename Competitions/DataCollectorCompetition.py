df795e2f3d33270b6d464ae2780ca7154d360ae8
Ferretsroq <Ferretsroq@gmail.com>
2016-08-24 17:15:16 -0400

    Github Why
    Don’t even know what these changes are, but apparently I never
    committed them.

---
+++ Competitions/DataCollectorCompetition.py
@@ -0,0 +1,24 @@
+from pokemon_competitions import *
+
+pokemonList = [0]
+alternateFormesList = GetFormeData()
+
+'''Populates the pokemonList with every pokemon'''
+for dexNumber in range(720):
+    pokemonList.append(Pokemon(GetData(dexNumber+1)))
+    print pokemonList[dexNumber+1].thisPokemonName.decode('utf8')
+
+pokemonList = pokemonList + alternateFormesList
+
+'''Creates an ordered list, ordered by Battle Spot ranking'''
+orderedListByRank = orderByRanking(pokemonList)
+print orderedListByRank
+
+print "Length of ordered list: %d" % len(orderedListByRank)
+'''Writes the data to text files'''
+for x in orderedListByRank:
+    print x.thisPokemonRanking, x.thisPokemonName.decode('utf8'), x.totalNumberOfThisPokemon
+
+dataFile = shelve.open('./Data')
+dataFile['data'] = orderedListByRank
+dataFile.close()
